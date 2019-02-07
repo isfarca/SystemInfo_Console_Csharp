f04faa25e05f1ee42536b9afbf7acd47e2c6c835
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 19:17:19 +0100

    Adding project.

---
+++ SystemInfo/SystemInfo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gpuInfo = new SystemInfo.GraphicsAdapterInfo();
+            var cpuInfo = new SystemInfo.ProcessorInfo();
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine(" Graphics Adapter Info");
+            Console.WriteLine("------------------------");
+
+            foreach(PropertyInfo prop in gpuInfo.GetType().GetProperties())
+            {
+                Console.WriteLine(String.Format("{0}: {1}", prop.Name, prop.GetValue(gpuInfo, null)));
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("    Processor Info");
+            Console.WriteLine("------------------------");
+
+            foreach (PropertyInfo prop in cpuInfo.GetType().GetProperties())
+            {
+                Console.WriteLine(String.Format("{0}: {1}", prop.Name, prop.GetValue(cpuInfo, null)));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SystemInfo/SystemInfo/SystemInfo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Management;
+
+namespace SystemInfo
+{
+    public class GraphicsAdapterInfo
+    {
+        #region Properties
+
+        public string Name { get; private set; }        
+        public uint HorizontalResolution { get; private set; }
+        public uint VerticalResolution { get; private set; }
+        public uint BitsPerPixel { get; private set; }
+        public uint RefreshRate { get; private set; }
+        public string DriverVersion { get; private set; }        
+
+        #endregion
+
+        #region Constructor
+
+        public GraphicsAdapterInfo()
+        {
+            Refresh();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Refresh()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+            ManagementObjectCollection collection = searcher.Get();
+            ManagementObjectCollection.ManagementObjectEnumerator enumerator = collection.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+                return;
+
+            ManagementBaseObject graphicsAdapter = enumerator.Current;
+
+            this.Name = (string)graphicsAdapter["Name"];
+            this.RefreshRate = (uint)graphicsAdapter["CurrentRefreshRate"];
+            this.HorizontalResolution = (uint)graphicsAdapter["CurrentHorizontalResolution"];
+            this.VerticalResolution = (uint)graphicsAdapter["CurrentVerticalResolution"];
+            this.BitsPerPixel = (uint)graphicsAdapter["CurrentBitsPerPixel"];
+            this.DriverVersion = (string)graphicsAdapter["DriverVersion"];
+        }
+
+        #endregion
+    }
+
+    public class ProcessorInfo
+    {
+        #region Enums
+
+        public enum ProcessorArchitecture
+        {
+            x86 = 0,
+            MIPS = 1,
+            Alpha = 2,
+            PowerPC = 3,
+            Itanium = 6,
+            x64 = 9
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; private set; }
+        public string Manufacturer { get; private set; }
+        public ProcessorArchitecture Architecture { get; private set; }        
+        public uint Cores { get; private set; }
+        public uint ClockSpeed { get; private set; }        
+        public uint L2CacheSize { get; private set; }
+        public uint L3CacheSize { get; private set; }
+        public string Version { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public ProcessorInfo()
+        {
+            Refresh();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Refresh()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            ManagementObjectCollection collection = searcher.Get();
+            ManagementObjectCollection.ManagementObjectEnumerator enumerator = collection.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+                return;
+
+            ManagementBaseObject processor = enumerator.Current;
+
+            this.Name = (string)processor["Name"];
+            this.Manufacturer = (string)processor["Manufacturer"];
+            this.Architecture = (ProcessorArchitecture)Enum.Parse(typeof(ProcessorArchitecture), processor["Architecture"].ToString());
+            this.Version = (string)processor["Version"];
+            this.Cores = (uint)processor["NumberOfCores"];
+            this.ClockSpeed = (uint)processor["CurrentClockSpeed"];            
+            this.L2CacheSize = (uint)processor["L2CacheSize"];
+            this.L3CacheSize = (uint)processor["L3CacheSize"];
+        }
+
+        #endregion
+    }
+}
+
+    
